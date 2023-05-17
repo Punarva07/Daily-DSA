1eac96e2c035ee87b8d52c8b65515d7060fbbe95
Punarva07 <117620513+Punarva07@users.noreply.github.com>
2023-05-17 23:58:31 +0530

    Add files via upload

---
+++ LinearSearch.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+using namespace std;
+
+/*int main(){
+    int arr[10] = {5, 1 ,7 ,65, -9, 85, 46, 23, 2 , 12};
+    int key = 85;
+
+    for( int i = 0; i<10; i++)
+    {
+        if(key == arr[i])
+        {
+            cout<<"Key found at index: "<<i;
+        }
+    }
+
+
+    return 0;
+}*/
+
+bool search(int arr[], int size, int key) {
+
+    for( int i = 0; i<size; i++ ) {
+
+        if( arr[i] == key) {
+            return 1;
+        }
+
+    }
+    return 0;
+}
+
+int main() {
+
+    int arr[10] = { 5, 7, -2, 10, 22, -2, 0, 5, 22, 1};
+
+    cout <<" Enter the element to search for " << endl; 
+    int key;
+    cin >> key;
+
+    bool found = search(arr, 10, key);
+
+    if( found ) {
+        cout <<" Key is present "<< endl;
+    }
+    else{
+        cout <<" Key is absent " << endl;
+    }
+
+
+    return 0;
+}
